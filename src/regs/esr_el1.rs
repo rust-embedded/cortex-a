88f483909b3aa9d3a375d69eda1bc29ce43d9f25
Berkus Decker <berkus+github@metta.systems>
2020-10-12 22:49:18 +0300

    Add more exception classes to ESR_EL1

--- src/regs/esr_el1.rs
+++ src/regs/esr_el1.rs
@@ -4,6 +4,7 @@
 //
 // Author(s):
 //   - Andre Richter <andre.o.richter@gmail.com>
+//   - Berkus Decker <berkus+github@metta.systems>
 
 //! Exception Syndrome Register - EL1
 //!
@@ -25,19 +26,38 @@ register_bitfields! {u64,
         EC  OFFSET(26) NUMBITS(6) [
             Unknown               = 0b00_0000,
             TrappedWFIorWFE       = 0b00_0001,
+            TrappedMCRorMRC       = 0b00_0011, // A32
+            TrappedMCRRorMRRC     = 0b00_0100, // A32
+            TrappedMCRorMRC2      = 0b00_0101, // A32
+            TrappedLDCorSTC       = 0b00_0110, // A32
             TrappedFP             = 0b00_0111,
+            TrappedMRRC           = 0b00_1100, // A32
+            BranchTarget          = 0b00_1101,
             IllegalExecutionState = 0b00_1110,
+            SVC32                 = 0b01_0001, // A32
             SVC64                 = 0b01_0101,
             HVC64                 = 0b01_0110,
             SMC64                 = 0b01_0111,
             TrappedMsrMrs         = 0b01_1000,
+            TrappedSve            = 0b01_1001,
+            PointerAuth           = 0b01_1100,
             InstrAbortLowerEL     = 0b10_0000,
             InstrAbortCurrentEL   = 0b10_0001,
             PCAlignmentFault      = 0b10_0010,
             DataAbortLowerEL      = 0b10_0100,
             DataAbortCurrentEL    = 0b10_0101,
             SPAlignmentFault      = 0b10_0110,
-            TrappedFP64           = 0b10_1100
+            TrappedFP32           = 0b10_1000, // A32
+            TrappedFP64           = 0b10_1100,
+            SError                = 0b10_1111,
+            BreakpointLowerEL     = 0b11_0000,
+            BreakpointCurrentEL   = 0b11_0001,
+            SoftwareStepLowerEL   = 0b11_0010,
+            SoftwareStepCurrentEL = 0b11_0011,
+            WatchpointLowerEL     = 0b11_0100,
+            WatchpointCurrentEL   = 0b11_0101,
+            Bkpt32                = 0b11_1000, // A32 BKTP instruction
+            Brk64                 = 0b11_1100  // A64 BRK instruction
         ],
 
         /// Instruction Length for synchronous exceptions.
