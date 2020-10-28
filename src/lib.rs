a1e95407217f0547ca8d9c45a443d33b6118b8f3
Andre Richter <andre.o.richter@gmail.com>
2020-10-28 11:13:38 +0100

    Bump version; Update README

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,46 @@
 // Author(s):
 //   - Andre Richter <andre.o.richter@gmail.com>
 
-//! Low level access to Cortex-A processors
+//! Low level access to Cortex-A processors.
 //!
-//! This crate provides:
+//! ## Currently Supported Architectures
 //!
-//! - Safe wrappers around assembly instructions
+//! - [x] AArch64
+//! - [ ] AArch32
 //!
-//! For now, there's not much. I will update it gradually.
-//! If you want to contribute, feel free to reach out!
+//! ## Minimum Supported Rust Version
 //!
-//! # Minimum Supported Rust Version (MSRV)
+//! Requires rustc 1.45.0 or later due to use of the new `asm!()` syntax.
 //!
-//! This crate is guaranteed to compile on stable Rust 1.39 and up. It *might* compile with older
-//! versions but that may change in any new patch release.
+//! ## Usage
+//!
+//! Example from https://github.com/rust-embedded/rust-raspberrypi-OS-tutorials
+//!
+//! ```rust
+//! unsafe fn el2_to_el1_transition() -> ! {
+//!     // Enable timer counter registers for EL1.
+//!     CNTHCTL_EL2.write(CNTHCTL_EL2::EL1PCEN::SET + CNTHCTL_EL2::EL1PCTEN::SET);
+//!
+//!     // No offset for reading the counters.
+//!     CNTVOFF_EL2.set(0);
+//!
+//!     // Set EL1 execution state to AArch64.
+//!     HCR_EL2.write(HCR_EL2::RW::EL1IsAarch64);
+//!
+//!     // Set up a simulated exception return.
+//!     SPSR_EL2.write(
+//!         SPSR_EL2::D::Masked
+//!             + SPSR_EL2::A::Masked
+//!             + SPSR_EL2::I::Masked
+//!             + SPSR_EL2::F::Masked
+//!             + SPSR_EL2::M::EL1h,
+//!     );
+//! ```
+//!
+//! ## Disclaimer
+//!
+//! Descriptive comments in the source files are taken from the
+//! [ARM Architecture Reference Manual ARMv8, for ARMv8-A architecture profile](https://static.docs.arm.com/ddi0487/ca/DDI0487C_a_armv8_arm.pdf?_ga=2.266626254.1122218691.1534883460-1326731866.1530967873).
 
 #![feature(core_intrinsics)]
 #![feature(custom_inner_attributes)]
