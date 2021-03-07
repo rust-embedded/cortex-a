acc8a25cecfa9be01acef78f6f8da077bf8f6090
Andre Richter <andre.o.richter@gmail.com>
2021-03-07 13:38:47 +0100

    Clippy: Allow upper case acronyms

--- src/lib.rs
+++ src/lib.rs
@@ -42,6 +42,7 @@
 //! Descriptive comments in the source files are taken from the
 //! [ARM Architecture Reference Manual ARMv8, for ARMv8-A architecture profile](https://static.docs.arm.com/ddi0487/ca/DDI0487C_a_armv8_arm.pdf?_ga=2.266626254.1122218691.1534883460-1326731866.1530967873).
 
+#![allow(clippy::clippy::upper_case_acronyms)]
 #![feature(core_intrinsics)]
 #![feature(custom_inner_attributes)]
 #![feature(asm)]
