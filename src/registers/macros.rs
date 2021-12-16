06950d997f02cede432a0ab861f16c142e007626
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-12-16 22:40:48 +0100

    Use full path to asm! macro
    Rust 1.59 stabilizes the asm feature and the explicit feature flag is
    not required anymore. However, the macros were not using the full path
    and this is causing a failure with rustc 1.59 in nightly.

--- src/registers/macros.rs
+++ src/registers/macros.rs
@@ -15,7 +15,7 @@ macro_rules! __read_raw {
                 () => {
                     let reg;
                     unsafe {
-                        asm!(concat!($asm_instr, " {reg:", $asm_width, "}, ", $asm_reg_name), reg = out(reg) reg, options(nomem, nostack));
+                        core::arch::asm!(concat!($asm_instr, " {reg:", $asm_width, "}, ", $asm_reg_name), reg = out(reg) reg, options(nomem, nostack));
                     }
                     reg
                 }
@@ -37,7 +37,7 @@ macro_rules! __write_raw {
                 #[cfg(target_arch = "aarch64")]
                 () => {
                     unsafe {
-                        asm!(concat!($asm_instr, " ", $asm_reg_name, ", {reg:", $asm_width, "}"), reg = in(reg) value, options(nomem, nostack))
+                        core::arch::asm!(concat!($asm_instr, " ", $asm_reg_name, ", {reg:", $asm_width, "}"), reg = in(reg) value, options(nomem, nostack))
                     }
                 }
 
