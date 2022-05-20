71ea227bb383e9c925ca3a6cdc46a03c3a0dc80b
Andre Richter <andre.o.richter@gmail.com>
2022-05-20 08:45:36 +0200

    Fix clippy warnings on host build; Wrap comments at 100

--- src/asm/random.rs
+++ src/asm/random.rs
@@ -5,34 +5,33 @@
 // Author(s):
 //   - Ali Saidi <alisaidi@amazon.com>
 
+#[cfg(target_arch = "aarch64")]
+use core::arch::asm;
 
 /// Implement an interface for accessing Arm v8.5 RNG instructions.
-/// An empty struct is used to confirm that the system has the
-/// instructions available.
-/// # Example:
+///
+/// An empty struct is used to confirm that the system has the instructions available.
+///
+/// # Example
+///
 /// ```no_run
 /// use cortex_a::asm::random::ArmRng;
 /// if let Some(rng) = ArmRng::new() {
-///    let rand_num = rng.rndr();
+///     let rand_num = rng.rndr();
 /// }
 /// ```
 #[derive(Copy, Clone, Debug)]
 pub struct ArmRng;
 
-use crate::registers::ID_AA64ISAR0_EL1;
-use core::arch::asm;
-use tock_registers::interfaces::Readable;
-
 impl ArmRng {
-    /// Return an empty object that is used to gate calling
-    /// rndr and rndrss on discovery of the feature so each
-    /// call doesn't need to confirm it.
+    /// Return an empty object that is used to gate calling rndr and rndrss on discovery of the
+    /// feature so each call doesn't need to confirm it.
+    #[cfg(target_arch = "aarch64")]
     #[inline]
     pub fn new() -> Option<Self> {
-        #[cfg(not(target_arch = "aarch64"))]
-        return None;
+        use crate::registers::ID_AA64ISAR0_EL1;
+        use tock_registers::interfaces::Readable;
 
-        #[cfg(target_arch = "aarch64")]
         if ID_AA64ISAR0_EL1.is_set(ID_AA64ISAR0_EL1::RNDR) {
             Some(ArmRng)
         } else {
@@ -40,15 +39,21 @@ impl ArmRng {
         }
     }
 
+    #[cfg(not(target_arch = "aarch64"))]
+    pub fn new() -> Option<Self> {
+        None
+    }
+
     /// Return an random number from the Arm v8.5 RNG.
-    /// This returns an option because the instruction can fail
-    /// (e.g. the entropy is exhausted or the RNG has failed.)
+    ///
+    /// This returns an option because the instruction can fail (e.g. the entropy is exhausted or
+    /// the RNG has failed.)
+    #[cfg(target_arch = "aarch64")]
     #[inline]
     pub fn rndr(&self) -> Option<u64> {
         let mut flags: u64;
         let mut data: u64;
 
-        #[cfg(target_arch = "aarch64")]
         unsafe {
             asm!(
                 "mrs {o}, s3_3_c2_c4_0",
@@ -57,22 +62,29 @@ impl ArmRng {
                 f = out(reg) flags,
                 options(nomem, nostack));
         }
-        if cfg!(not(target_arch = "aarch64")) || flags != 0 {
+
+        if flags != 0 {
             None
         } else {
             Some(data)
         }
     }
 
-    /// Return an random number from the Arm v8.5 RNG after reseeding it
-    /// This returns an option because the instruction can fail
-    /// (e.g. the entropy is exhausted or the RNG has failed.)
+    #[cfg(not(target_arch = "aarch64"))]
+    pub fn rndr(&self) -> Option<u64> {
+        None
+    }
+
+    /// Return a random number from the Arm v8.5 RNG after reseeding it.
+    ///
+    /// This returns an option because the instruction can fail (e.g. the entropy is exhausted or
+    /// the RNG has failed.)
+    #[cfg(target_arch = "aarch64")]
     #[inline]
     pub fn rndrss(&self) -> Option<u64> {
         let mut flags: u64;
         let mut data: u64;
 
-        #[cfg(target_arch = "aarch64")]
         unsafe {
             asm!(
                 "mrs {o}, s3_3_c2_c4_1",
@@ -82,16 +94,18 @@ impl ArmRng {
                 options(nomem, nostack));
         }
 
-        if cfg!(not(target_arch = "aarch64")) || flags != 0 {
+        if flags != 0 {
             None
         } else {
             Some(data)
         }
     }
 
-
+    #[cfg(not(target_arch = "aarch64"))]
+    pub fn rndrss(&self) -> Option<u64> {
+        None
+    }
 }
-
 
 #[cfg(all(test, target_os = "linux"))]
 mod tests {
@@ -99,13 +113,11 @@ mod tests {
 
     #[test]
     pub fn test_rndr() {
-        // This works on Linux from userspace since Linux emulatates
-        // the Arm ID registers on the userspace undef.
+        // This works on Linux from userspace since Linux emulatates the Arm ID registers on the
+        // userspace undef.
         if let Some(rand) = ArmRng::new() {
             assert!(rand.rndr().unwrap() != 0);
             assert!(rand.rndrss().unwrap() != 0);
         }
     }
-
 }
-
