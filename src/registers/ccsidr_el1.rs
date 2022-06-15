ff8127cbb4e7abd0844e6e5b5c2248f60f40f680
Valentin B <valentin.be@protonmail.com>
2022-06-15 23:36:23 +0200

    Add CCSIDR_EL1 getters/setters with respect to CCIDX availability

--- src/registers/ccsidr_el1.rs
+++ src/registers/ccsidr_el1.rs
@@ -15,43 +15,30 @@ use tock_registers::{
 };
 
 register_bitfields! {u64,
-    /// The representation of `CCSIDR_EL1` when `FEAT_CCIDX` is implemented.
-    pub CCSIDR_EL1_WITH_FEAT_CCIDX [
-        /// Number of sets in cache.
-        ///
-        /// A value of 0 indicates 1 set in the cache. The number does not
-        /// necessarily have to be a power of 2.
-        NumSets OFFSET(32) NUMBITS(24) [],
-
-        /// Associativity of cache.
-        ///
-        /// A value of 0 indicates an associativity of 1. The value does not
-        /// necessarily have to be a power of 2.
-        Associativity OFFSET(3) NUMBITS(21) [],
-
-        /// Log2(Number of bytes in cache lline) - 4.
-        ///
-        /// **Examples:**
-        ///
-        /// - For a line length of 16 bytes: Log2(16) - 4 = 0. This is the minimum line length.
-        ///
-        /// - For a line length of 32 bytes: Log2(32) - 4 = 1.
-        LineSize OFFSET(0) NUMBITS(3) []
-    ],
-
-    /// The representation of `CCSIDR_EL1` otherwise.
     pub CCSIDR_EL1 [
         /// Number of sets in cache.
         ///
         /// A value of 0 indicates 1 set in the cache. The number does not
         /// necessarily have to be a power of 2.
-        NumSets OFFSET(13) NUMBITS(15) [],
+        NumSetsWithCCIDX OFFSET(32) NUMBITS(24) [],
+
+        /// Number of sets in cache.
+        ///
+        /// A value of 0 indicates 1 set in the cache. The number does not
+        /// necessarily have to be a power of 2.
+        NumSetsWithoutCCIDX OFFSET(13) NUMBITS(15) [],
 
         /// Associativity of cache.
         ///
         /// A value of 0 indicates an associativity of 1. The value does not
         /// necessarily have to be a power of 2.
-        Associativity OFFSET(3) NUMBITS(10) [],
+        AssociativityWithCCIDX OFFSET(3) NUMBITS(21) [],
+
+        /// Associativity of cache.
+        ///
+        /// A value of 0 indicates an associativity of 1. The value does not
+        /// necessarily have to be a power of 2.
+        AssociativityWithoutCCIDX OFFSET(3) NUMBITS(10) [],
 
         /// Log2(Number of bytes in cache lline) - 4.
         ///
@@ -64,21 +51,55 @@ register_bitfields! {u64,
     ]
 }
 
-pub struct RegWithFeatCcidx;
-pub struct Reg;
+#[inline(always)]
+fn has_feature_ccidx() -> bool {
+    use crate::registers::ID_AA64MMFR2_EL1;
 
-impl Readable for RegWithFeatCcidx {
-    type T = u64;
-    type R = CCSIDR_EL1_WITH_FEAT_CCIDX::Register;
-
-    sys_coproc_read_raw!(u64, "CCSIDR_EL1", "x");
+    ID_AA64MMFR2_EL1.read(ID_AA64MMFR2_EL1::CCIDX) != 0
 }
 
-impl Writeable for RegWithFeatCcidx {
-    type T = u64;
-    type R = CCSIDR_EL1_WITH_FEAT_CCIDX::Register;
+pub struct Reg;
 
-    sys_coproc_write_raw!(u64, "CCSIDR_EL1", "x");
+impl Reg {
+    /// Reads the [`CCSIDR_EL1`] `NumSets` field, selecting the correct
+    /// bit field by checking if the running CPU supports `CCIDX`.
+    #[inline(always)]
+    pub fn get_num_sets(&self) -> u64 {
+        match has_feature_ccidx() {
+            true => self.read(CCSIDR_EL1::NumSetsWithCCIDX),
+            false => self.read(CCSIDR_EL1::NumSetsWithoutCCIDX),
+        }
+    }
+
+    /// Sets the [`CCSIDR_EL1`] `NumSets` field, selecting the correct
+    /// bit field by checking if the running CPU supports `CCIDX`.
+    #[inline(always)]
+    pub fn set_num_sets(&self, value: u64) {
+        match has_feature_ccidx() {
+            true => self.write(CCSIDR_EL1::NumSetsWithCCIDX.val(value)),
+            false => self.write(CCSIDR_EL1::NumSetsWithoutCCIDX.val(value)),
+        }
+    }
+
+    /// Reads the [`CCSIDR_EL1`] `Associativity` field, selecting the correct
+    /// bit field by checking if the running CPU supports `CCIDX`.
+    #[inline(always)]
+    pub fn get_associativity(&self) -> u64 {
+        match has_feature_ccidx() {
+            true => self.read(CCSIDR_EL1::AssociativityWithCCIDX),
+            false => self.read(CCSIDR_EL1::AssociativityWithoutCCIDX),
+        }
+    }
+
+    /// Sets the [`CCSIDR_EL1`] `Associativity` field, selecting the correct
+    /// bit field by checking if the running CPU supports `CCIDX`.
+    #[inline(always)]
+    pub fn set_associativity(&self, value: u64) {
+        match has_feature_ccidx() {
+            true => self.write(CCSIDR_EL1::AssociativityWithCCIDX.val(value)),
+            false => self.write(CCSIDR_EL1::AssociativityWithoutCCIDX.val(value)),
+        }
+    }
 }
 
 impl Readable for Reg {
@@ -95,5 +116,4 @@ impl Writeable for Reg {
     sys_coproc_write_raw!(u64, "CCSIDR_EL1", "x");
 }
 
-pub const CCSIDR_EL1_WITH_FEAT_CCIDX: RegWithFeatCcidx = RegWithFeatCcidx;
 pub const CCSIDR_EL1: Reg = Reg;
