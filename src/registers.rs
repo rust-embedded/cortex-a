163a4ccc660733525a89275b2eb0c48b1582ec44
Ali Saidi <alisaidi@amazon.com>
2022-09-01 11:32:35 -0500

    Add the VTCR_EL2 register

--- src/registers.rs
+++ src/registers.rs
@@ -66,6 +66,7 @@ mod ttbr0_el2;
 mod ttbr1_el1;
 mod vbar_el1;
 mod vbar_el2;
+mod vtcr_el2;
 
 pub use actlr_el1::ACTLR_EL1;
 pub use actlr_el2::ACTLR_EL2;
@@ -127,3 +128,4 @@ pub use ttbr0_el2::TTBR0_EL2;
 pub use ttbr1_el1::TTBR1_EL1;
 pub use vbar_el1::VBAR_EL1;
 pub use vbar_el2::VBAR_EL2;
+pub use vtcr_el2::VTCR_EL2;
